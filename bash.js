cc6b0082e85dee101731dbb8fce3aac5ed0bc7b1
njasi <nick@jasinski3.com>
2019-09-23 12:51:02 -0500

    fixed done()

--- bash.js
+++ bash.js
@@ -1,13 +1,14 @@
+module.exports = done;
+function done(output){
+  process.stdout.write(output)
+  process.stdout.write("\nprompt > ")
+}
+
 const pwd = require("./pwd.js")
 const ls = require("./ls.js")
 const cat = require('./cat.js')
 const curl = require('./curl.js')
 
-module.exports = done;
-function done(output){
-  process.stdout.write(output)
-  process.stdout.write("prompt > ")
-}
 
 process.stdout.write("prompt > ");
 process.stdin.on('data', (test) => {
--- pwd.js
+++ pwd.js
@@ -1,6 +1,5 @@
 module.exports = pwd;
 const done = require ("./bash.js");
-console.log(done)
 function pwd(cmd){
   if(cmd === "pwd"){
     done(process.cwd());
